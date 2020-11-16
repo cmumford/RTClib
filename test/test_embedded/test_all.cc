34353d82ea984902f6f5dc12956a46e587ddb1ec
Chris Mumford <cmumford@cmumford.com>
2020-11-15 20:20:11 -0800

    Broke up test function into individual functions.

--- test/test_embedded/test_all.cc
+++ test/test_embedded/test_all.cc
@@ -9,11 +9,20 @@ constexpr i2c_port_t kRTCI2CPort = TUNER1_I2C_PORT;
  * Max of 1MHz recommended by:
  * https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/i2c.html#_CPPv4N12i2c_config_t9clk_speedE
  */
-constexpr uint16_t kI2CClockHz = 100000;
+constexpr int kI2CClockHz = 100000;
 
 SemaphoreHandle_t g_i2c_mutex;
 
-void test_init_rtc() {
+namespace {
+
+std::unique_ptr<rtc::DS3231> CreateClock() {
+  std::unique_ptr<rtc::I2CMaster> master(
+      new rtc::I2CMaster(kRTCI2CPort, g_i2c_mutex));
+  std::unique_ptr<rtc::DS3231> rtc(new rtc::DS3231(std::move(master)));
+  return rtc;
+}
+
+esp_err_t InitializeI2C() {
   const i2c_config_t config = {
       .mode = I2C_MODE_MASTER,
       .sda_io_num = I2C_SDA_GPIO,
@@ -24,32 +33,57 @@ void test_init_rtc() {
   };
 
   esp_err_t err = i2c_param_config(kRTCI2CPort, &config);
-  TEST_ASSERT_EQUAL(ESP_OK, err);
+  if (err != ESP_OK)
+    return err;
 
-  err = i2c_driver_install(kRTCI2CPort, I2C_MODE_MASTER, 0, 0, 0);
-  TEST_ASSERT_EQUAL(ESP_OK, err);
+  return i2c_driver_install(kRTCI2CPort, I2C_MODE_MASTER, 0, 0, 0);
 }
 
-void test_rtc() {
-  std::unique_ptr<rtc::I2CMaster> master(
-      new rtc::I2CMaster(kRTCI2CPort, g_i2c_mutex));
-  std::unique_ptr<rtc::DS3231> rtc(new rtc::DS3231(std::move(master)));
+}  // namespace
+
+void test_init_rtc() {
+  // TODO: Move to setup().
+  TEST_ASSERT_EQUAL(ESP_OK, InitializeI2C());
+}
+
+void test_set_and_get_date() {
+  auto rtc = CreateClock();
   TEST_ASSERT_NOT_NULL(rtc);
   TEST_ASSERT_TRUE(rtc->begin());
 
   const rtc::DateTime dt(2020, 11, 14, 21, 26, 59);
   TEST_ASSERT_TRUE(rtc->adjust(dt));
 
+  // This is a possible race condition as there is "daylight" between the set
+  // and the get call, and the time could change.
   const rtc::DateTime now = rtc->now();
   TEST_ASSERT_TRUE(dt.operator==(now));
+}
 
-  const float temp = rtc->getTemperature();
-  TEST_ASSERT_GREATER_OR_EQUAL(0, temp);
+void test_32k() {
+  auto rtc = CreateClock();
+  TEST_ASSERT_NOT_NULL(rtc);
+  TEST_ASSERT_TRUE(rtc->begin());
 
   rtc->enable32K();
   TEST_ASSERT_TRUE(rtc->isEnabled32K());
   rtc->disable32K();
   TEST_ASSERT_FALSE(rtc->isEnabled32K());
+}
+
+void test_temperature() {
+  auto rtc = CreateClock();
+  TEST_ASSERT_NOT_NULL(rtc);
+  TEST_ASSERT_TRUE(rtc->begin());
+
+  const float temp = rtc->getTemperature();
+  TEST_ASSERT_GREATER_OR_EQUAL(0, temp);
+}
+
+void test_square_pin_mode() {
+  auto rtc = CreateClock();
+  TEST_ASSERT_NOT_NULL(rtc);
+  TEST_ASSERT_TRUE(rtc->begin());
 
   TEST_ASSERT_TRUE(rtc->writeSqwPinMode(rtc::DS3231_OFF));
   TEST_ASSERT_EQUAL(rtc::DS3231_OFF, rtc->readSqwPinMode());
@@ -73,7 +107,10 @@ void process() {
   UNITY_BEGIN();
 
   RUN_TEST(test_init_rtc);
-  RUN_TEST(test_rtc);
+  RUN_TEST(test_set_and_get_date);
+  RUN_TEST(test_32k);
+  RUN_TEST(test_temperature);
+  RUN_TEST(test_square_pin_mode);
 
   UNITY_END();
 }
