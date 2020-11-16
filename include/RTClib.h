34e0a9e962708233405b525e0bdb200f4165b12d
Chris Mumford <cmumford@cmumford.com>
2020-11-16 15:06:26 -0800

    Moved PCF8523 definition into rtc_pcf8523.h

--- include/RTClib.h
+++ include/RTClib.h
@@ -29,83 +29,10 @@
 #include "rtc_ds1307.h"
 #include "rtc_ds3231.h"
 #include "rtc_i2c.h"
+#include "rtc_pcf8523.h"
 #include "rtc_timespan.h"
 
 namespace rtc {
-
-/** PCF8523 INT/SQW pin mode settings */
-enum Pcf8523SqwPinMode {
-  PCF8523_OFF = 7,             /**< Off */
-  PCF8523_SquareWave1HZ = 6,   /**< 1Hz square wave */
-  PCF8523_SquareWave32HZ = 5,  /**< 32Hz square wave */
-  PCF8523_SquareWave1kHz = 4,  /**< 1kHz square wave */
-  PCF8523_SquareWave4kHz = 3,  /**< 4kHz square wave */
-  PCF8523_SquareWave8kHz = 2,  /**< 8kHz square wave */
-  PCF8523_SquareWave16kHz = 1, /**< 16kHz square wave */
-  PCF8523_SquareWave32kHz = 0  /**< 32kHz square wave */
-};
-
-/** PCF8523 Timer Source Clock Frequencies for Timers A and B */
-enum PCF8523TimerClockFreq {
-  PCF8523_Frequency4kHz = 0,   /**< 1/4096th second = 244 microseconds,
-                                    max 62.256 milliseconds */
-  PCF8523_Frequency64Hz = 1,   /**< 1/64th second = 15.625 milliseconds,
-                                    max 3.984375 seconds */
-  PCF8523_FrequencySecond = 2, /**< 1 second, max 255 seconds = 4.25 minutes */
-  PCF8523_FrequencyMinute = 3, /**< 1 minute, max 255 minutes = 4.25 hours */
-  PCF8523_FrequencyHour = 4,   /**< 1 hour, max 255 hours = 10.625 days */
-};
-
-/** PCF8523 Timer Interrupt Low Pulse Width options for Timer B only */
-enum PCF8523TimerIntPulse {
-  PCF8523_LowPulse3x64Hz = 0,  /**<  46.875 ms   3/64ths second */
-  PCF8523_LowPulse4x64Hz = 1,  /**<  62.500 ms   4/64ths second */
-  PCF8523_LowPulse5x64Hz = 2,  /**<  78.125 ms   5/64ths second */
-  PCF8523_LowPulse6x64Hz = 3,  /**<  93.750 ms   6/64ths second */
-  PCF8523_LowPulse8x64Hz = 4,  /**< 125.000 ms   8/64ths second */
-  PCF8523_LowPulse10x64Hz = 5, /**< 156.250 ms  10/64ths second */
-  PCF8523_LowPulse12x64Hz = 6, /**< 187.500 ms  12/64ths second */
-  PCF8523_LowPulse14x64Hz = 7  /**< 218.750 ms  14/64ths second */
-};
-
-/** PCF8523 Offset modes for making temperature/aging/accuracy adjustments */
-enum Pcf8523OffsetMode {
-  PCF8523_TwoHours = 0x00, /**< Offset made every two hours */
-  PCF8523_OneMinute = 0x80 /**< Offset made every minute */
-};
-
-/**************************************************************************/
-/*!
-    @brief  RTC based on the PCF8523 chip connected via I2C and the Wire library
-*/
-/**************************************************************************/
-class PCF8523 {
- public:
-  PCF8523(std::unique_ptr<I2CMaster> i2c);
-
-  bool begin(void);
-  bool adjust(const DateTime& dt);
-  bool lostPower(void);
-  bool initialized(void);
-  DateTime now();
-  bool start(void);
-  bool stop(void);
-  bool isrunning();
-  Pcf8523SqwPinMode readSqwPinMode();
-  bool writeSqwPinMode(Pcf8523SqwPinMode mode);
-  bool enableSecondTimer(void);
-  bool disableSecondTimer(void);
-  bool enableCountdownTimer(PCF8523TimerClockFreq clkFreq,
-                            uint8_t numPeriods,
-                            uint8_t lowPulseWidth);
-  bool enableCountdownTimer(PCF8523TimerClockFreq clkFreq, uint8_t numPeriods);
-  bool disableCountdownTimer(void);
-  bool deconfigureAllTimers(void);
-  bool calibrate(Pcf8523OffsetMode mode, int8_t offset);
-
- private:
-  std::unique_ptr<I2CMaster> i2c_;
-};
 
 /** PCF8563 CLKOUT pin mode settings */
 enum Pcf8563SqwPinMode {
