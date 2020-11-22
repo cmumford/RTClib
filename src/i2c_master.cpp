bbf39f24d3ae133c5a7c79a25b71708ac8877c12
Chris Mumford <cmumford@cmumford.com>
2020-11-21 21:07:14 -0800

    I2C: Implemented Ping().

--- src/i2c_master.cpp
+++ src/i2c_master.cpp
@@ -13,9 +13,27 @@
 namespace rtc {
 
 namespace {
+
 constexpr char TAG[] = "I2C-master";
 constexpr bool ACK_CHECK_EN = true;
 constexpr TickType_t kI2CCmdWaitTicks = 1000 / portTICK_RATE_MS;
+
+i2c_cmd_handle_t StartWriteCommand(uint8_t slave_addr) {
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  if (!cmd)
+    return nullptr;
+  esp_err_t err = i2c_master_start(cmd);
+  if (err == ESP_OK) {
+    err = i2c_master_write_byte(cmd, (slave_addr << 1) | I2C_MASTER_WRITE,
+                                ACK_CHECK_EN);
+  }
+  if (err != ESP_OK) {
+    i2c_cmd_link_delete(cmd);
+    return nullptr;
+  }
+  return cmd;
+}
+
 }  // namespace
 
 // Static
@@ -63,8 +81,26 @@ bool I2CMaster::ReadRegister(uint8_t addr, uint8_t reg, uint8_t* val) {
 }
 
 bool I2CMaster::Ping(uint8_t addr) {
-  // TODO: Implement
-  return true;
+  i2c_cmd_handle_t cmd = StartWriteCommand(addr);
+  if (!cmd)
+    return false;
+  esp_err_t err = i2c_master_stop(cmd);
+  if (err != ESP_OK)
+    goto PING_DONE;
+
+  if (i2c_mutex_)
+    xSemaphoreTake(i2c_mutex_, portMAX_DELAY);
+
+  err = i2c_master_cmd_begin(i2c_num_, cmd, kI2CCmdWaitTicks);
+
+  if (i2c_mutex_)
+    xSemaphoreGive(i2c_mutex_);
+
+PING_DONE:
+  if (err != ESP_OK)
+    ESP_LOGE(TAG, "Ping 0x%x failed: %s", addr, esp_err_to_name(err));
+  i2c_cmd_link_delete(cmd);
+  return err == ESP_OK;
 }
 
 std::unique_ptr<I2COperation> I2CMaster::CreateWriteOp(uint8_t slave_addr,
